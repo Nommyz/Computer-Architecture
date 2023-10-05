b48bef9c62e8689f7f649f2f0ff73a9bddce19fe
Ice <teeraphat147258@gmail.com>
2023-10-05 11:23:57 +0700

    bug fixed

--- Simulator/Simulator.java
+++ Simulator/Simulator.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Simulator {
     private static final int NUMMEMORY = 65536; // maximum number of words in memory
@@ -9,10 +12,10 @@ public class Simulator {
 
 
     public static class StateStruct{
-        int pc;
+        int pc = 0;
         int[] mem = new int[NUMMEMORY];
         int[] reg = new int[NUMREGS];
-        int numMemory;
+        int numMemory ;
     }
 
     public static void printState(StateStruct state){
@@ -30,20 +33,61 @@ public class Simulator {
     System.out.println("end state\n");
     }
     
-    public static void main(String[] args){
-        String namefile = args[0];
+    public static void main(String[] args) throws FileNotFoundException {
+//        try {
+//            StateStruct state = new StateStruct();
+//            printState(state);
+//        }catch (Error e){
+//            System.out.println("Error");
+//        }
         StateStruct state = new StateStruct();
 
-        state.pc;
-        int[] argu = new int[3];
+
+        try {
+            File myText = new File("Simulator/machine-code.txt");
+            Scanner s = new Scanner(myText);
+            while (s.hasNextLine()) {
+                String data = s.nextLine();
+                state.mem[state.numMemory] = Integer.parseInt(data);
+                System.out.println(state.mem[state.numMemory]+ "\n");
+                state.numMemory++;
+
+            }
+        }catch (FileNotFoundException E){
+            System.out.println("Error Files not found");
+            E.printStackTrace();
+            System.exit(1);
+        }
+
+
+
+
+
+//        try (BufferedReader reader = new BufferedReader(new FileReader(namefile))) {
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//                state.mem[state.numMemory] = Integer.parseInt(line);
+//                System.out.println("memory[" + state.numMemory + "]=" + state.mem[state.numMemory]);
+//                state.numMemory++;
+//            }
+//        } catch (IOException E) {
+//            System.err.println("error: can't open files " + namefile);
+//            E.printStackTrace();
+//            System.exit(1);
+//        }
+
+
+
+        state.pc = 0;
+        int[] arg = new int[3];
         int RegA , RegB , DestReg;
-        for(int i = 1; i != 1; i++){
-            switch (mem[pc] >> 22){
+        for(int i = 1; i <= 999 ; i++){
+            switch (state.mem[state.pc] >> 22){
                 case 0: //add 000
-                    RFormat(mem[pc],arg);
+                    RFormat(state.mem[state.pc],arg);
                     RegA = state.reg[arg[0]];
                     RegB = state.mem[arg[1]];
-                    state.reg[arg[2]] = RegA+RegB;
+                    state.reg[arg[2]] = RegA + RegB;
                     break;
                 case 1: //nand 001
                     break;
@@ -61,12 +105,44 @@ public class Simulator {
                     break;
             }
         }
-
-        private static void RFormat(int BitNum , int [] arg){ //Rformat(ยังงงๆอยู่)
-            arg[0] = (BitNum & (7 << 19 )) >> 19; // RegA เอา bit ที่ 22-20
-            arg[1] = (BitNum & (7 << 16 )) >> 16; // RegB เอา bit ที่ 19-17
-            arg[2] = BitNum & 7; // DestReg เอา bit ที่ 2-0
-        }
         
+    }
+
+    /**
+     * using this method to assign Bits into arg for RFormat
+     * @param BitNum using BitNum to calculate logic and assign to arg
+     * @param arg using arg for waiting BitNum to calculate logic
+     */
+    private static void RFormat(int BitNum, int[] arg){ //r-format
+        arg[0] = (BitNum & (7 << 19 )) >> 19; // regA เอา bit ที่ 21-19
+        arg[1] = (BitNum & (7 << 16 )) >> 16; // regB เอา bit ที่ 18-16
+        arg[2] = BitNum & 7; // destReg เอา bit ที่ 2-0
+    }
+
+    /**
+     * using this method to assign Bits into arg for IFormat
+     * @param BitNum using BitNum to calculate logic and assign to arg
+     * @param arg using arg for waiting BitNum to calculate logic
+     */
+    private static void IFormat(int BitNum, int[] arg){
+
+    }
+
+    /**
+     * using this method to assign Bits into arg for JFormat
+     * @param BitNum using BitNum to calculate logic and assign to arg
+     * @param arg using arg for waiting BitNum to calculate logic
+     */
+    private static void JFormat(int BitNum, int[] arg){
+
+    }
+
+    /**
+     * using this method to assign Bits into arg for OFormat
+     * @param BitNum using BitNum to calculate logic and assign to arg
+     * @param arg using arg for waiting BitNum to calculate logic
+     */
+    private static void OFormat(int BitNum, int[] arg){
+
     }
 }
